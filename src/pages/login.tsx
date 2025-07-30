5062897e5eea62086576b7a8b3bef7b0d3b7dd57
João Lucas <joao.2023014171@alunocesul.com>
2025-07-29 22:36:05 -0300

    Adicionação de baguios

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,19 @@
+import { Flex, Image } from "@chakra-ui/react";
+import loginImage from "../../public/assets/login-image.gif";
+
 export default function Login() {
     return (
-        <div>
-            Login
-        </div>
+        <Flex w="100vw" h="100vh">
+          <Flex w="50%" bg="blue.400" align="center" justify="center">
+
+            <Image w={500} h={500} src={loginImage.src}/>
+
+          </Flex>
+            <Flex w="50%" bg="green.400">
+            </Flex>
+
+        </Flex>
+
+
     )
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,13 +3,15 @@ import { defineConfig } from "@chakra-ui/react";
 export const themeConfig = defineConfig({
   globalCss: {
     "html, body": {
-      backgroundColor: "blue",
+      backgroundColor: "#8e3e16",
     },
   },
   theme: {
     tokens: {
-      colors: {},
+      colors: {
+        },
+      },
     },
   },
-});
+);
 
