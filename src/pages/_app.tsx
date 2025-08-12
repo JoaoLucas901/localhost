8ea128ee20cedf2372d5b7073d1d625a446cbd35
João Lucas <joao.2023014171@alunocesul.com>
2025-08-12 19:42:53 -0300

    primeira add de session

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import { Provider } from "@/components/ui/provider"
+import { SessionProvider } from "@/contexts/SessionContext"
+import { Session } from "inspector/promises"
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps ) {
   return (
     <Provider>
+      <SessionProvider>
       <Component {...pageProps} />
+      </SessionProvider>
     </Provider>
   )
 }
