92f6e327d97d915f8f0d60fc645e4cb31c821573
João Lucas <joao.2023014171@alunocesul.com>
2025-09-02 20:39:24 -0300

    criação de botões para a dashboard

--- src/components/DefaultHeader/Menu.tsx
+++ src/components/DefaultHeader/Menu.tsx
@@ -1,5 +1,11 @@
-import { Button, CloseButton, Drawer, IconButton, Kbd, Portal } from "@chakra-ui/react";
-import { IoMdMenu } from "react-icons/io";
+import { Button, CloseButton, Drawer, IconButton, Portal, VStack } from "@chakra-ui/react";
+import { IoIosBook, IoMdMenu, IoMdPeople } from "react-icons/io";
+import { RiDashboard2Fill } from "react-icons/ri";
+import { NavigationItem } from "./NativationItem";
+import { TiThumbsOk } from "react-icons/ti";
+import { FaGraduationCap } from "react-icons/fa";
+import { MdLibraryBooks } from "react-icons/md";
+import { GrNotes } from "react-icons/gr";
 
 export function Menu() {
   return (
@@ -19,8 +25,24 @@ export function Menu() {
             </Drawer.Header>
 
             <Drawer.Body>
-              Press the <Kbd>Esc</Kbd> key or click outside the drawer to close it.
+              <VStack>
+              <NavigationItem icon={<RiDashboard2Fill />} label="Dashboard" />
+
+              <NavigationItem icon={<IoMdPeople />} label="Estudantes" />
+
+              <NavigationItem icon={<FaGraduationCap />} label="Professores" />
+
+              <NavigationItem icon={<IoIosBook />} label="Cursos" />
+
+              <NavigationItem icon={<MdLibraryBooks />} label="Notas" />
+
+              <NavigationItem icon={<GrNotes />} label="Matrículas" />
+
+              <NavigationItem icon={<TiThumbsOk />} label="Lagarto Bird" />
+
+              </VStack>
             </Drawer.Body>
+
 
             <Drawer.Footer>
               <Button variant="outline">Cancel</Button>
